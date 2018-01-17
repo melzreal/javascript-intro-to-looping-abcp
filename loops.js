f6fde53794b25475de7617f428a4bc4a6846fcb4
Melissa Real <melissa.real@hotmail.com>
2018-01-17 20:20:23 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,44 @@
+
+
+function forLoop(phrases){
+  
+ 
+  
+  for (let i = 0; i < 25; i++) {
+
+  if(i===1){
+    phrases.push('I am 1 strange loop.');
+  } else{
+  
+    phrases.push( 'I am"+ {i} +" strange loop' ); 
+
+    }
+  } return phrases;
+  
+}
+
+function whileLoop(x) {
+  
+   
+  while(x > 0){
+    console.log("yay" + x);
+    x--;
+  }
+  return ('done');
+}
+ 
+ 
+ function doWhileLoop(arr) {
+  
+ function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+ arr.pop();
+ 
+} while (arr.length > 0 && maybeTrue());
+
+ return arr;  
+ }
+ 
