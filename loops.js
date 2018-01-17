17ae943a5afbd78814bcced808a67ad13daa0225
Melissa Real <melissa.real@hotmail.com>
2018-01-17 12:50:49 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+
+
+function forLoop(phrases){
+  
+  for (let i = 0; i < 25; i++) {
+
+  if(i===1){
+    phrases.push("I am 1 strange loop.");
+  } else{
+  
+    phrases.push( "I am"+ [i] +" strange loop" ); 
+
+    }
+  }
+}
